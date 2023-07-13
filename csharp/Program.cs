56cc3b0d23fd571a32390f12565c002c507f09c2
Alex Morales <44649358+msalemor@users.noreply.github.com>
2023-07-13 10:09:16 -0400

    Update Program.cs

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -4,7 +4,7 @@
 // touch .env
 // code .
 
-// Get an OpenAI API key and URL from the Azure Portal
+// Getting an OpenAI API key and URL from the Azure Portal
 // Calling an API from requests.http
 // Converting the prompt and completion json to c# records using Bing
 
@@ -20,87 +20,110 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using dotenv.net;
 
-// Step 1 - Some setup
-// Note: Take a look at the request.http file from VS code to see how to POST using REST client and build the supporting objects
-const string SYSTEM_MESSAGE = "You are a general assistant";
-var prompt_tokens = 0;
-var completion_tokens = 0;
-var history = new List<Message>();
+// Step 1 - App Configuration
+const string DEFAULT_SYSTEM_MESSAGE = "You are a general assistant. Greet me by saying \"Hello, I am your chatbot assistant. How may I be of service?\"";
+var conversation = new List<Message>();
 
-// Step 2 - Get the OpenAI KEY and URI
-// TODO: Add a .env file and add the API KEY and URI
+// Step 2 - Read API key and URI from either .env file or environment variables
 DotEnv.Load();
-var api_key = Environment.GetEnvironmentVariable("OPENAI_KEY");
-var uri = Environment.GetEnvironmentVariable("OPENAI_URI");
-if (api_key is null || uri is null)
+string apikey = Environment.GetEnvironmentVariable("OPENAI_KEY") ?? "";
+string uri = Environment.GetEnvironmentVariable("OPENAI_URI") ?? "";
+
+if (uri == string.Empty || apikey == string.Empty)
 {
-    Console.WriteLine("Please add the OPENAI_KEY and OPENAI_URI to the .env file and run again.");
+    Console.WriteLine("Please set OPENAI_KEY and OPENAI_URI in your .env file or environment variables.");
     return;
 }
 
-// Step 3 - Configure the HttpClient (acting like a singleton for the life of the application)
+// Step 3 - Configure the HttpClient headers to use the api key
 var client = new HttpClient();
 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-client.DefaultRequestHeaders.Add("api-key", api_key);
+client.DefaultRequestHeaders.Add("api-key", $"{apikey}");
+
+// Step 4 - Call the bot for the first time
+conversation.Add(new Message(Role.system.ToString(), DEFAULT_SYSTEM_MESSAGE));
+var ans = await GetGptCompletionAsync(conversation);
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine(ans.Item1 + "\n");
+var ctokens = ans.Item2;
+var ptokens = ans.Item3;
 
 
-// Step 4 - Activate the bot for the first time
-history.Add(new Message("system", SYSTEM_MESSAGE));
-var ans = await GetCompletionAsync(history);
-if (ans.Item1 is not null)
-{
-    history.Add(new Message("system", ans.Item1));
-    Console.WriteLine(ans.Item1);
-    Console.WriteLine("Type 'quit' to exit the application or 'history' to look at the history\n");
-    prompt_tokens = +ans.Item2;
-    completion_tokens = +ans.Item3;
-}
-
-// Step 5 - Enter the loop
+// Step 5 - Loop through the conversation having the ability to exit
+var firstTime = true;
 while (true)
 {
-    Console.WriteLine($"Tokens In: {prompt_tokens} Out: {completion_tokens} --> What is your question?\n");
-    var prompt = Console.ReadLine();
-    if (string.Compare(prompt, "quit", StringComparison.InvariantCultureIgnoreCase) == 0)
+    if (!firstTime)
     {
-        Console.WriteLine("Goodbye");
+        ColorWrite($"Tokens In:{ptokens} Out:{ctokens} -> What is your question?\n");
+    }
+    else
+    {
+        firstTime = false;
+    }
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    var prompt = Console.ReadLine();
+    var lprompt = prompt?.ToLower();
+    if (lprompt == "exit" || lprompt == "quit")
+    {
+        ColorWrite("Goodbye!");
         break;
     }
-    if (string.Compare(prompt, "history", StringComparison.InvariantCultureIgnoreCase) == 0)
+    else if (lprompt == "history")
     {
-        foreach (var item in history)
+        ColorWrite("\nConversation History:\n", ConsoleColor.Yellow);
+        foreach (var message in conversation)
         {
-            Console.WriteLine($"{item.Role}: {item.Content}");
+            ColorWrite("Role: ", ConsoleColor.Blue, false);
+            ColorWrite($"{message.Role}", ConsoleColor.Green, false);
+            ColorWrite(" Content: ", ConsoleColor.Blue, false);
+            var outMsg = message.Content.Length < 60 ? message.Content : message.Content.Substring(0, 60) + "...";
+            ColorWrite($"{outMsg}", ConsoleColor.Green);
+
         }
+        ColorWrite("");
         continue;
     }
-    //Console.WriteLine(prompt);
-    history.Add(new Message("user", prompt ?? string.Empty));
-    ans = await GetCompletionAsync(history);
-    if (ans.Item1 is not null)
+    else if (prompt is not null)
     {
-        history.Add(new Message("system", ans.Item1));
-        Console.WriteLine("\n" + ans.Item1 + "\n");
-        prompt_tokens = +ans.Item2;
-        completion_tokens = +ans.Item3;
+        conversation.Add(new Message(Role.user.ToString(), prompt));
+        ans = await GetGptCompletionAsync(conversation);
+        ColorWrite("\n" + ans.Item1 + "\n", ConsoleColor.Green);
+
+        conversation.Add(new Message(Role.system.ToString(), ans.Item1 ?? ""));
+        ctokens += ans.Item2;
+        ptokens += ans.Item3;
     }
 }
 return;
 
 // Supporting classes and methods
-async Task<Tuple<string?, int, int, int>> GetCompletionAsync(List<Message> history)
+void ColorWrite(string text, ConsoleColor color = ConsoleColor.White, bool newLine = true)
 {
-    var prompt = new Prompt(history, 100, 0.3d);
+    Console.ForegroundColor = color;
+    if (newLine)
+    {
+        Console.WriteLine(text);
+    }
+    else
+    {
+        Console.Write(text);
+    }
+}
+
+async Task<Tuple<string?, int, int>> GetGptCompletionAsync(List<Message> history)
+{
+    var prompt = new Prompt(history, 1000, 0.3d);
     var json = JsonSerializer.Serialize(prompt);
     var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-    // Note: We could have retry logic here
+    // Note: We could add retry logic here
     var response = await client.PostAsync(new Uri(uri), content);
 
     if (!response.IsSuccessStatusCode)
     {
         Console.WriteLine($"Error: {response.StatusCode}");
-        return new Tuple<string?, int, int, int>(null, 0, 0, 0);
+        return new Tuple<string?, int, int>(null, 0, 0);
     }
 
     var completion = await response.Content.ReadAsStringAsync();
@@ -111,34 +134,42 @@ async Task<Tuple<string?, int, int, int>> GetCompletionAsync(List<Message> histo
         var choice = completionObject.Choices[0];
         if (choice is not null)
         {
-            return new Tuple<string?, int, int, int>(choice.Message.Content,
+            return new Tuple<string?, int, int>(choice.Message.Content,
                 completionObject.Usage.CompletionTokens,
-                completionObject.Usage.PromptTokens,
-                completionObject.Usage.TotalTokens);
+                completionObject.Usage.PromptTokens);
         }
     }
-    return new Tuple<string?, int, int, int>(null, 0, 0, 0); ;
+    return new Tuple<string?, int, int>(null, 0, 0);
 }
 
-public record Message([property: JsonPropertyName("role")] string Role,
+record Message([property: JsonPropertyName("role")] string Role,
     [property: JsonPropertyName("content")] string Content);
 
-public record Prompt([property: JsonPropertyName("messages")] List<Message> Messages,
+record Prompt([property: JsonPropertyName("messages")] List<Message> Messages,
     [property: JsonPropertyName("max_tokens")] int Max_tokens,
-    [property: JsonPropertyName("temperature")] double temperature);
+    [property: JsonPropertyName("temperature")] double temperature,
+    [property: JsonPropertyName("n")] int N = 1,
+    [property: JsonPropertyName("stop")] string? Stop = null
+    );
 
-public record Choice([property: JsonPropertyName("role")] int Index,
+record Choice([property: JsonPropertyName("role")] int Index,
     [property: JsonPropertyName("finish_reason")] string FinishReason,
     [property: JsonPropertyName("message")] Message Message);
 
-public record Usage([property: JsonPropertyName("completion_tokens")] int CompletionTokens,
+record Usage([property: JsonPropertyName("completion_tokens")] int CompletionTokens,
     [property: JsonPropertyName("prompt_tokens")] int PromptTokens,
     [property: JsonPropertyName("total_tokens")] int TotalTokens);
 
-public record Completion(
+record Completion(
     [property: JsonPropertyName("role")] string Id,
     [property: JsonPropertyName("object")] string Object,
     [property: JsonPropertyName("created")] long Created,
     [property: JsonPropertyName("model")] string Model,
     [property: JsonPropertyName("choices")] List<Choice> Choices,
     [property: JsonPropertyName("usage")] Usage Usage);
+
+enum Role
+{
+    system,
+    user
+}
